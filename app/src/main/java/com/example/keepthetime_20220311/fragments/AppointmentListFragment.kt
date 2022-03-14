5be56f13c13bc5291c004bbf0932d2a25b992526
이진실 <ljsil1204@naver.com>
2022-03-14 11:41:05 +0900

    연습문제 - 약속목록프래그먼트에 BaseFragment, 데이터바인딩 세팅 적용

--- app/src/main/java/com/example/keepthetime_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/example/keepthetime_20220311/fragments/AppointmentListFragment.kt
@@ -4,21 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.keepthetime_20220311.R
+import com.example.keepthetime_20220311.databinding.FragmentAppointmentListBinding
+import com.example.keepthetime_20220311.datas.BasicResponse
+import com.example.keepthetime_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class AppointmentListFragment : Fragment() {
+class AppointmentListFragment : BaseFragment() {
+
+    lateinit var binding : FragmentAppointmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_appointment_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_appointment_list, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        binding.txtTest.text = "코틀린에서 문구 바꾸기"
 
     }
 }
