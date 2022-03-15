f80c5a3ce5431947310cf051b0ccae5c7c567a26
이진실 <ljsil1204@naver.com>
2022-03-15 12:22:37 +0900

    뷰페이저2의 어댑터 클래스 제작 > 뷰페이저 2와 연결

--- app/src/main/java/com/example/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.example.keepthetime_20220311
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.example.keepthetime_20220311.adapters.MainViewPagerAdapter
+import com.example.keepthetime_20220311.adapters.MainViewPager2Adapter
 import com.example.keepthetime_20220311.databinding.ActivityMainBinding
-import com.example.keepthetime_20220311.datas.BasicResponse
-import com.example.keepthetime_20220311.utils.ContextUtil
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -28,6 +22,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.mainViewPager2.adapter = MainViewPager2Adapter( this ) // 변수 : Activity => 객체 : Context로 대입불가.
 
     }
 }
