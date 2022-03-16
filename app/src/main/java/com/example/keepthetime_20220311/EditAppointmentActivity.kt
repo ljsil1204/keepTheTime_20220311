7dca97afa663db75992bb01f7aa7039fc7f0dfbc
이진실 <ljsil1204@naver.com>
2022-03-16 14:49:03 +0900

    첫번째 추천 경로의 모든 정거장을 좌표로 담아서, 경로선 재 설정

--- app/src/main/java/com/example/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/EditAppointmentActivity.kt
@@ -245,7 +245,7 @@ class EditAppointmentActivity : BaseActivity() {
 
 //                coord ~ 선택한 latLng 까지 대중교통 경로를 그려보자. (PathOverlay 기능 활용) + ODsay 라이브러리 활용
 
-                val myODsayService = ODsayService.init(mContext, "gw9m7KtTSb97PJwg3C/jQx+OKMjdKmugQKqgeBp44Vk")
+                val myODsayService = ODsayService.init(mContext, "2ZUL48uEwaiRd+EIpRbEVg2bSQS/bZnhQXCDOqczKlM")
 
                 myODsayService.requestSearchPubTransPath(
                     coord.longitude.toString(),
@@ -259,17 +259,57 @@ class EditAppointmentActivity : BaseActivity() {
                         override fun onSuccess(p0: ODsayData?, p1: API?) {
 
                             val jsonObj = p0!!.json!!
-
                             Log.d("길찾기응답", jsonObj.toString())
 
                             val resultObj = jsonObj.getJSONObject("result")
-
                             Log.d("result", resultObj.toString())
 
-                            val pathArr = resultObj.getJSONObject("path") // 여러 추천 경로 중 첫번째 만 사용해보자.
+                            val pathArr = resultObj.getJSONArray("path") // 여러 추천 경로 중 첫번째 만 사용해보자.
 
                             val firstPathObj = pathArr.getJSONObject(0)
                             Log.d("첫번째 경로",firstPathObj.toString())
+
+//                           첫번째 경로를 지나는 모든 정거장들의 위경도값을 담을 목록
+                            val stationLatLngList = ArrayList<LatLng>()
+
+
+//                            불광~강남 : 도보 5분 / 지하철 30분 / 버스 30분 / 도보 5분
+                            val subPathArr = firstPathObj.getJSONArray("subPath")
+
+                            for (i in 0 until subPathArr.length()) {
+
+                                val subPathObj = subPathArr.getJSONObject(i)
+
+//                                둘러보려는 경로가, 정거장 목록을 내려준다면 (지하철 or 버스) => 내부 파싱
+
+                                if (!subPathObj.isNull("passStopList")) {
+
+                                    val passStopListObj = subPathObj.getJSONObject("passStopList")
+                                    val stationsArr = passStopListObj.getJSONArray("stations")
+
+
+//                                    실제 정거장 목록 파싱 => 각 정거장의 위도/경도 추출 가능. => ArrayList에 담아서, 경로선의 좌표
+                                    for (j in 0 until  stationsArr.length()) {
+
+                                        val stationObj = stationsArr.getJSONObject(j)
+
+//                                        위도 (y좌표) , 경도 (x좌표) 추출
+                                        val lat = stationObj.getString("y") .toDouble()
+                                        val lng = stationObj.getString("x").toDouble()
+
+//                                        네이버 지도의 좌표로 만들어서 > ArrayList에 담자.
+                                        stationLatLngList.add(LatLng(lat,lng))
+
+                                    }
+
+                                }
+
+
+                            }
+
+//                            완성된 정거장 경로들을 => path의 경로로 재설정. 지도에 새로 반영.
+                            path!!.coords = stationLatLngList
+                            path!!.map = naverMap
 
                         }
 
