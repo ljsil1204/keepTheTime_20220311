0350257d0d57da514e4e3a389d2236a073699597
이진실 <ljsil1204@naver.com>
2022-03-17 16:14:40 +0900

    페북 로그인 - 돌아오면 할 일 (아직 비어있음) / 실제 로그인 실행

--- app/src/main/java/com/example/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/SignInActivity.kt
@@ -12,11 +12,16 @@ import com.example.keepthetime_20220311.databinding.ActivitySignInBinding
 import com.example.keepthetime_20220311.datas.BasicResponse
 import com.example.keepthetime_20220311.utils.ContextUtil
 import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.kakao.sdk.user.UserApiClient
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class SignInActivity : BaseActivity() {
 
@@ -37,6 +42,26 @@ class SignInActivity : BaseActivity() {
         binding.btnFacebookLogin.setOnClickListener {
 
 //            페북 로그인 기능 실행
+
+//            1. 로그인 하고 다녀오면 어떤 행동을 할지? 인터페이스 설정.
+            LoginManager.getInstance().registerCallback(mCallbackManager, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            })
+
+//            2. 실제로 페북 로그인 실행
+//            공개 프로필 / 이메일 주소 받아와달라.
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
 
         }
 
@@ -141,6 +166,7 @@ class SignInActivity : BaseActivity() {
 
     }
 
+//    페북 로그인
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         mCallbackManager.onActivityResult(requestCode,resultCode,data)
         super.onActivityResult(requestCode, resultCode, data)
