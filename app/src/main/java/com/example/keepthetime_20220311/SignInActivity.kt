dd76ee65ca35db833f43df13c3c2f76ad162e58a
이진실 <ljsil1204@naver.com>
2022-03-17 16:40:15 +0900

    돌아온 토큰 값으로, 페북서버에 다시 내 정보 조회 요청   - 받아온 정보를 로그로

--- app/src/main/java/com/example/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/SignInActivity.kt
@@ -11,10 +11,7 @@ import com.example.keepthetime_20220311.api.ServerAPI
 import com.example.keepthetime_20220311.databinding.ActivitySignInBinding
 import com.example.keepthetime_20220311.datas.BasicResponse
 import com.example.keepthetime_20220311.utils.ContextUtil
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
+import com.facebook.*
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.kakao.sdk.user.UserApiClient
@@ -51,11 +48,19 @@ class SignInActivity : BaseActivity() {
 //                    페북로그인 -> 페북 서버의 토큰 값 받기.
                     Log.d("페북로그인성공", result?.accessToken.toString())
 
-//                    받은 토큰으로 > 내 정보도 받아오자.
+//                    받은 토큰으로 > 내 정보도 받아오자. => GraghRequest 클래스 활용.
                     
 //                    1. 정보를 받아오면 뭘 할건지? 인터페이스 설정
+                    val graghRequest = GraphRequest.newMeRequest(result?.accessToken, object : GraphRequest.GraphJSONObjectCallback{
+                        override fun onCompleted(jsonObj: JSONObject?, response: GraphResponse?) {
+
+                            Log.d("받아온 정보", jsonObj!!.toString())
+
+                        }
+                    })
                     
 //                    2.실제로 요청 호출
+                    graghRequest.executeAsync()
 
                 }
 
@@ -176,13 +181,13 @@ class SignInActivity : BaseActivity() {
 
     }
 
-//    페북 로그인
+//    페북 로그인 화면에 다녀오면 할 일 세팅.
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         mCallbackManager.onActivityResult(requestCode,resultCode,data)
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-//    카카오톡
+//    카카오 서버에서, 로그인된 계정의 정보 불러오기
     fun getKakaoUserInfo() {
         
         UserApiClient.instance.me { user, error -> 
